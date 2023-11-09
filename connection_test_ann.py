a2d4e79c98eed17e5ccdb18784562ca2614834b8
Aaron James Ridley <ridley@umich.edu>
2023-11-09 13:55:10 -0500

    FEAT: testing different components

---
+++ connection_test_ann.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import logging
+import signal
+import scipy
+import numpy
+import pickle
+from time import sleep
+from datetime import datetime, timedelta
+import smtplib, ssl
+from config import config, skyscan_config
+from schedule import observations
+import serial
+
+
+import utilities.time_helper
+from utilities.image_taker import Image_Helper
+#from utilities.send_mail import SendMail
+
+from components.camera import getCamera
+from components.shutterhid import HIDLaserShutter
+#from components.sky_scanner import SkyScanner
+#from components.skyalert import SkyAlert
+from components.powercontrol import PowerControl
+#from components.filterwheel import FilterWheel
+
+
+
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+# powerControl = PowerControl(config['powerSwitchAddress'], config['powerSwitchUser'], config['powerSwitchPassword'])
+# powerControl.turnOn(config['AndorPowerPort'])
+# powerControl.turnOn(config['SkyScannerPowerPort'])
+# powerControl.turnOn(config['LaserPowerPort'])
+
+# logging.info('Initializing LaserShutter')
+lasershutter = HIDLaserShutter(config['vendorId'], config['productId'])
+
+print('closing shutter...')
+lasershutter.close_shutter()
+sleep(2)
+print('opening shutter for 30s ...')
+lasershutter.open_shutter()
+sleep(30)
+print('closing shutter...')
+lasershutter.close_shutter()
+sleep(5)
+
+exit()
+
+# fw = FilterWheel('/dev/ttyUSB0')
+# fw.home()
+
+# logging.info('Initializing SkyScanner')
+# skyscanner = SkyScanner(skyscan_config['max_steps'], skyscan_config['azi_offset'], skyscan_config['zeni_offset'], skyscan_config['azi_world'], skyscan_config['zeni_world'], skyscan_config['number_of_steps'], skyscan_config['port_location'])
+# logging.info('Sending SkyScanner home')
+# skyscanner.go_home()
+
+logging.info('Initializing CCD')
+camera = getCamera("Andor")
+print(camera.getTemperature())
+camera.shutDown()
+
+# sa = SkyAlert(config['skyAlertAddress'])
+# logging.info(sa.getList())
+
+# powerControl.turnOff(config['AndorPowerPort'])
+# powerControl.turnOff(config['SkyScannerPowerPort'])
+# powerControl.turnOff(config['LaserPowerPort'])
+
+# sm = SendMail(config['email'], config['pickleCred'], config['gmailCred'], config['site'])
+
+# print("sending mail")
+# sm.send_error(["khanhn2@illinois.edu"], "Test connection")
