2e7afc811e66b88fe678e3106c203b6b458399be
Aaron James Ridley <ridley@umich.edu>
2024-08-15 08:30:10 -0400

    sunrise and sunset seem to be off

--- utilities/time_helper.py
+++ utilities/time_helper.py
@@ -9,19 +9,30 @@ class TimeHelper:
     site_location = ephem.Observer()
 
     def __init__(self):
-        print(config)
         self.site_location.lat = config["latitude"]
         self.site_location.lon = config["longitude"]
+        self.site_location.utcdate = datetime.utcnow()
         self.site_location.date = datetime.now()
         self.site_location.elevation = config["elevation"]
         self.site_location.horizon = config["horizon"]
         self.sun = ephem.Sun()
+        self.offset = 43.0
 
     def getSunrise(self):
         return ephem.localtime(self.site_location.next_rising(self.sun))
 
+    def getSunriseUtcCorrect(self):
+        valEphem = self.site_location.next_rising(self.sun)
+        valEphemD = valEphem.datetime() + timedelta(minutes = self.offset)
+        return valEphemD
+
     def getSunset(self):
         return ephem.localtime(self.site_location.next_setting(self.sun))
+
+    def getSunsetUtcCorrect(self):
+        valEphem = self.site_location.next_setting(self.sun)
+        valEphemD = valEphem.datetime() + timedelta(minutes = self.offset)
+        return valEphemD
 
     def getHousekeeping(self):
         return self.getSunset() - timedelta(minutes=config["startHousekeeping"])
