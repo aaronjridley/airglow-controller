d7179d878012af910ad830121e48d4195c38cf5a
Aaron James Ridley <ridley@umich.edu>
2024-08-27 08:31:41 -0400

    modified to UM system

--- shutdown_peripherals.py
+++ shutdown_peripherals.py
@@ -1,16 +1,26 @@
+#!/usr/bin/python3
+
 from components.powercontrol import PowerControl
-
 from config import config
+from time import sleep
 
-# Code to shutdown the peripherals that need to be off during the day (CCD, Laser, SkyScanner)
-# This is a hedge against the code crashing and not going through the power down sequence
+# Code to shutdown the peripherals that need to be off during the day
+# (CCD, Laser, SkyScanner) This is a hedge against the code crashing
+# and not going through the power down sequence
 
-try:
-    powerControl = PowerControl(config['powerSwitchAddress'], config['powerSwitchUser'], config['powerSwitchPassword'])
-    powerControl.turnOff(config['AndorPowerPort'])
-    powerControl.turnOff(config['SkyScannerPowerPort'])
-    powerControl.turnOff(config['LaserPowerPort'])
-    powerControl.turnOff(config['FilterWheelPowerPort'])
-except:
-    print("Error turning peripherals off")
-    
+powerControl = PowerControl(config['powerSwitchAddress'], \
+                            config['powerSwitchUser'], \
+                            config['powerSwitchPassword'])
+
+print(" -> Turning off ports")
+ports = []
+ports.append(config['UsbPowerPort'])
+ports.append(config['AndorPowerPort'])
+ports.append(config['LaserPowerPort'])
+ports.append(config['LaserShutterPowerPort'])
+ports.append(config['SledPowerPort'])
+for port in ports:
+    print('  --> Turning OFF port: ', port)
+    powerControl.turnOff(port)
+    sleep(2)
+
